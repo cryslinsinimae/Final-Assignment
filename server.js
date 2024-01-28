fc7bdfca0f9b3ab323579ab7fd1c0299fdc15142
cryslinsinimae <130795050+cryslinsinimae@users.noreply.github.com>
2024-01-28 02:47:17 +0200

    ToDoList

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./db');
+const todoRouter = require('./routes/todo.routes');
+const dotenv = require('dotenv');
+const app = express();
+
+app.use(bodyParser.json());
+
+db.connect((error) => {
+    if(error) {
+        console.log('Connection failed!');
+        console.log(error);
+    } else {
+        console.log('Connection initiated!');
+    } 
+});
+
+app.use('/todo', todoRouter );
+
+app.listen(3000, () => {
+    console.log('Server is running on Port 3000.');
+});
